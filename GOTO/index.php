d3d04b62e303c3988574c8b1f46620f10ba31631
Abhishek <abhisekgaire7@gmail.com>
2023-03-01 20:57:19 +0545

    main

---
+++ GOTO/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" type="text/css" href="./goto.css">
+</head>
+<body>
+    <div class="MAINDIV">
+        <div>
+            <ol>
+                <li ><a href="">WT PROJECT</a></li>
+            </ol>
+        </div>
+        <div class="div1">
+            <ol class="ol1">
+                <li ><a href="#">Home</a></li>
+                <li><a href="./Create/404.html">Team</a></li>
+                <li><a href="./Create/404.html">Learn</a></li>
+                <li><a href="./Create/404.html">Contact Us</a></li>
+            </ol>
+        </div>
+        <div class="div2">
+            <ol class="ol2">
+                <li><a href="../index.html">Log OUT</a></li>
+            </ol>
+        </div>
+    </div>
+</body>
+</html>
