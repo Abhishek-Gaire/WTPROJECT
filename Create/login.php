ba5904da60ccc0f35877aebd1404cedceeccdc0c
Abhishek <abhisekgaire7@gmail.com>
2023-03-03 12:40:01 +0545

    main

--- Create/login.php
+++ Create/login.php
@@ -1,3 +1,29 @@
+<?php
+require_once "../Main/save.php";
+error_reporting(0);
+
+session_start();
+
+$error = '';
+
+if(isset($_POST['submit'])){
+   $select = "SELECT * FROM Register WHERE Email = '$Email' && Password = '$Password'";
+   $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0){
+      $row = mysqli_fetch_array($result);
+
+      }elseif($row['user_type'] == 'user'){
+         $_SESSION['user_name'] = $row['name'];
+         header('location:user_dashboard.php');
+      }
+   }else{
+      $error = 'Incorrect email or password!';
+   }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,19 +35,13 @@
 </head>
 <body>
     <div class="div-form">
-        <form method="post" action="" id="Login_Form" enctype="multipart/form-data">
+        <form method="post" action="" id="Login_Form" enctype="multipart/form-data" onsubmit="return validatePassword()">
             <div>
                 <label for="Username">
                     Username:
                     <input type="text" id="username" name="UserName">
                 </label>
-            </div>
-            <div>
-                <label for="Email">
-                    Email:
-                    <input type="email" id="email" name="Email">
-                </label>
-            </div>
+            </div>         
             <div>
                 <label for="Password">
                     Password:
@@ -29,10 +49,15 @@
                 </label>
             </div>
             <div>
-                <button type="submit">Login</button>
+                <button type="submit" name="submit">Login</button>
             </div>
         </form>
         <h3>Don't have an account! <a href="./index.html">Register Now</a></h3>
     </div>
+    <script>
+        function validatePassword(){
+
+        }
+    </script>
 </body>
 </html>
---
+++ Main/save.php
@@ -0,0 +1,18 @@
+<?php 
+
+require_once "../utils/db.php";
+
+$FirstName=$_POST['FirstName'] ?? '';
+$LastName=$_POST['LastName'] ?? '';
+$UserName=$_POST['UserName'] ?? '';
+$Email=$_POST['Email'] ?? '';
+$Password=password_hash($_POST['Password'] ?? '', PASSWORD_DEFAULT);
+$ConfirmPassword=password_hash($_POST['ConfirmPassword'] ?? '', PASSWORD_DEFAULT);
+
+$sql="INSERT INTO Register(FirstName, LastName, Username, Email, Password, ConfirmPassword)
+VALUES('$FirstName' , '$LastName' , '$UserName' , '$Email' , '$Password' , '$ConfirmPassword')";
+
+mysqli_query($conn, $sql);
+header('<location: class="../GOTO/index.php">');
+
+
